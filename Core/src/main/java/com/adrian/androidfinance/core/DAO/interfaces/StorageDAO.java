231502e4602ed75719cf71fcf5266914ae0f4108
Severin Adrian <sadrian24@gmail.com>
2018-05-30 12:45:45 +0300

    Update DAO

--- Core/src/main/java/com/adrian/androidfinance/core/DAO/interfaces/StorageDAO.java
+++ Core/src/main/java/com/adrian/androidfinance/core/DAO/interfaces/StorageDAO.java
@@ -1,4 +1,12 @@
 package com.adrian.androidfinance.core.DAO.interfaces;
 
-public interface StorageDAO extends CommonDAO {
+import com.adrian.androidfinance.core.interfaces.Storage;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public interface StorageDAO extends CommonDAO<Storage> {
+    boolean addCurrency(Storage storage, Currency currency);
+    boolean deleteCurrency(Storage storage, Currency currency);
+    boolean updateCurrency(Storage storage, BigDecimal amount);
 }
